d9996de0795380829d45f99c61d638d03b0b30ea
chris.w.newman <chris.w.newman@accenturefederal.com>
2018-02-28 00:14:18 -0500

    adjusted input styles. added basic item search

--- src/app/items/items.module.ts
+++ src/app/items/items.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ItemInfoComponent } from 'app/items/item-info/item-info.component';
 import { ItemListComponent } from 'app/items/item-list/item-list.component';
 import { routing } from 'app/items/items.routing';
+import { FormsModule } from '@angular/forms';
+import { CoreModule } from 'app/core/core.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    CoreModule,
     routing
   ],
   declarations: [ItemListComponent, ItemInfoComponent]
