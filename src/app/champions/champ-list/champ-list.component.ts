ccf95b74cd1eff165ee49a8e446813bce40ddc55
chris.w.newman <chris.w.newman@accenturefederal.com>
2018-03-02 23:11:21 -0500

    improved responsive styling for champ list and champ info components.
    wip implementation -  select first result in query, and add form action to navigate

--- src/app/champions/champ-list/champ-list.component.ts
+++ src/app/champions/champ-list/champ-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Champion } from 'app/classes/champion';
 import { DataService } from 'app/core/data.service';
 import { HyperlinkingService } from 'app/core/hyperlinking.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-champ-list',
@@ -12,13 +13,37 @@ export class ChampListComponent implements OnInit {
   arrayOfKeys: any[];
   champions: Champion[];
   champQuery: string;
+  queryObservable: Observable<string>;
+  firstResult: any;
 
   constructor(protected loldata: DataService, protected common: HyperlinkingService) { }
 
   ngOnInit() {
     this.champions = Array.from(this.loldata.champions.values());
+
+    const sub = this.queryObservable.subscribe((x) => {
+      console.log(x);
+    });
   }
 
   // cool 1-line ES6 functions (instead of old 4-line ES5 functions)
   makeInfoURL = (champKey) => `/champions/${champKey}`;
+
+  // TODO: observable on champQuery
+  highlightFirstResult() {
+    if (this.champQuery !== '') {
+      console.log('getting firstResult');
+      // this.firstResult;
+      this.firstResult = transform(this.champions, 'name', this.champQuery)[0];
+      console.log(this.firstResult);
+    }
+
+
+    function transform(value, keys: string, term: string) {
+      if (!term) return value;
+      return (value || []).filter((item) => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
+    }
+  }
+
+  //
 }
