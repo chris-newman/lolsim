346b410c2b5c8b0248dc447d2a3af01ee8c0c698
chris.w.newman <chris.w.newman@accenturefederal.com>
2018-06-20 11:45:16 -0400

    improved image preloading behavior in build component.

--- src/app/core/hyperlinking.service.ts
+++ src/app/core/hyperlinking.service.ts
@@ -7,5 +7,7 @@ export class HyperlinkingService {
   constructor(protected loldata: DataService) { }
 
   public makeItemIconURL = (itemId) => `http://ddragon.leagueoflegends.com/cdn/${this.loldata.dataVersion}/img/item/${itemId}.png`;
+
   public makeChampIconURL = (champKey) => `http://ddragon.leagueoflegends.com/cdn/${this.loldata.dataVersion}/img/champion/${champKey}.png`;
+  public makeChampIconSrc = (champKey) => 'url(' + this.makeChampIconURL(champKey) + ')';
 }
--- src/app/simulation/build/build.component.ts
+++ src/app/simulation/build/build.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ChampSelectModalComponent } from '../../champions/champ-select-modal/champ-select-modal.component';
 import { Champion } from '../../classes/champion';
+import { HyperlinkingService } from '../../core/hyperlinking.service';
 
 @Component({
   selector: 'app-build',
@@ -12,43 +13,26 @@ import { Champion } from '../../classes/champion';
 export class BuildComponent implements OnInit {
   build: any;
   champBgImage: string;
-  show1 = false;
-  show2 = false;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private hyperlink: HyperlinkingService) { }
 
   ngOnInit() {
     this.build = {};
     this.champBgImage = 'none';
   }
 
-
-  private generateBgImage(champ: Champion): string {
-    console.log('generate bg image');
-    if (!champ) return 'none';
-    else if (champ && champ.key) {
-      console.log(champ);
-      // return  'url("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/'+champ.key+'_0.jpg")';
-      return `url("http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${champ.key}.png")`;
-    }
-  }
-
   openChampSelect() {
-    const modalRef = this.modalService.open(ChampSelectModalComponent, {
+    this.modalService.open(ChampSelectModalComponent, {
       windowClass: 'champ-select-modal',
       size: 'lg'
-    }).result
+    })
+    .result
       .then((champ) => {
-        console.log('.then');
-        console.log(champ);
         this.build.champ = champ;
-        this.champBgImage = this.generateBgImage(this.build.champ);
-        console.log(this.champBgImage);
+        this.champBgImage = this.hyperlink.makeChampIconSrc(this.build.champ.key);
       })
       .catch((err) => {
         console.warn(err);
       });
-    // modalRef.componentInstance.card = card;
   }
-
 }
